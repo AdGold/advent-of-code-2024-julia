5a42c4bf8b08e8075bb096e80369990a383fde08
Adrian Goldwaser <ad.goldwaser@gmail.com>
2024-12-10 14:59:24 +0000

    More day 10 cleanups

--- 10/main.jl
+++ 10/main.jl
@@ -15,22 +15,21 @@ function part2(data)
 end
 
 function part1_cleaned(data)
-    data = parse.(Int, data)
     dirs = CartesianIndex.([(0, 1), (1, 0), (0, -1), (-1, 0)])
-    recurse(pos, height) = vcat([dfs(pos + d, height + 1) for d in dirs]...)
-    dfs(pos, height) = pos ∉ keys(data) || height != data[pos] ? [] : height == 9 ? [pos] : recurse(pos, height)
-    sum(length.(Set.(dfs.(keys(data), 0))))
+    dfs(pos, h='0') = get(data, pos, 0) != h ? [] : h == '9' ? [pos] : vcat(dfs.(pos .+ dirs, h + 1)...)
+    sum(length∘Set∘dfs, keys(data))
 end
 
 function part2_cleaned(data)
-    data = parse.(Int, data)
-    recurse(pos, height) = sum(dfs.(CartesianIndex.([(0, 1), (1, 0), (0, -1), (-1, 0)]) .+ pos, height + 1))
-    dfs(pos, height) = pos ∉ keys(data) || height != data[pos] ? 0 : height == 9 ? 1 : recurse(pos, height)
-    sum(dfs.(keys(data), 0))
+    dirs = CartesianIndex.([(0, 1), (1, 0), (0, -1), (-1, 0)])
+    dfs(pos, h='0') = get(data, pos, 0) != h ? 0 : h == '9' ? 1 : sum(dfs.(pos .+ dirs, h + 1))
+    sum(dfs, keys(data))
 end
 
 data = stack(readlines("input.txt"))
+@assert part1(data) == part1_cleaned(data)
 @time println(part1(data))
 @time println(part1_cleaned(data))
+@assert part2(data) == part2_cleaned(data)
 @time println(part2(data))
 @time println(part2_cleaned(data))
